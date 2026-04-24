df09aef07be51996e12761874293ef99d93cdc08
anushreddy9346 <anushreddy970@gmail.com>
2026-04-24 15:39:33 +0530

    Update API URL to Render backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,15 @@
+// client/src/api/axios.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({
+  baseURL: 'https://your-app-name.onrender.com/api',  // ← Put your Render URL here
+});
 
-API.interceptors.request.use((req) => {
+// Auto-attach JWT token
+API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default API;
